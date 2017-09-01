e998447b24d4f61387e3cd965ce568d7fefa0ef2
suriing <suriing.diary@gmail.com>
2017-09-01 14:12:08 +0900

    Add commands : /read [text name], /list

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import time
 import datetime
 import configparser
 import telepot
+import subprocess
 
 # configparser for token & chat_id
 config = configparser.ConfigParser()
@@ -23,9 +24,26 @@ def handle(msg):
 	if chat_ids != chat_id:
 		bot.sendMessage(chat_ids, 'PERMISSION DENIED')
 		return
-	print('Command received: %s' % command)
-	write_down(command)
-	bot.sendMessage(chat_id, '\'' + command + '\'' + ' is saved')
+	if command[:5] == '/read':
+		if len(command) == 5:
+			doc_name = str(datetime.date.today()) + '.txt'
+		else:
+			doc_name = command[6:] + '.txt'
+			if os.path.isfile(dest_dir + doc_name):
+				ans = subprocess.check_output("cat " + dest_dir + doc_name, shell=True)
+				bot.sendMessage(chat_id, ans)
+			else:
+				bot.sendMessage(chat_id, doc_name + ' is not exist')
+	elif command == '/list':
+		text_list = []
+		for files in os.listdir(dest_dir):
+			if files.endswith(".txt"):
+				text_list.append(files[:-4])
+		bot.sendMessage(chat_id,"\n".join(text_list))
+	else:
+		print('Command received: %s' % command)
+		write_down(command)
+		bot.sendMessage(chat_id, '\'' + command + '\'' + ' is saved')
 
 bot = telepot.Bot(token)
 bot.message_loop(handle)
