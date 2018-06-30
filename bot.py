dddad700b9dc008171d677bf84da59e4837b5121
suriing <suriing.diary@gmail.com>
2018-07-01 00:37:56 +0900

    Rev for asustor

--- bot.py
+++ bot.py
@@ -1,43 +1,34 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python
 
 import sys
 import os
 import time
 import datetime
 import subprocess
-from urllib.parse import unquote
+from urllib import unquote
 from pprint import pprint
 import telepot
-import configparser
+import ConfigParser
 
 # Set variables
-config = configparser.ConfigParser()
+config = ConfigParser.ConfigParser()
 config.read(sys.argv[1])
 token = config.get('settings','token')
 chat_ids = int(config.get('settings','chat_id'))
 dest_dir = config.get('settings','dest_dir')
-tm_n = config.get('settings','tm_n')
-tm_n_wd = config.get('settings','tm_n_wd')
 tm_d1 = config.get('settings','tm_d1')
 tm_d2 = config.get('settings','tm_d2')
 tm_dt1 = config.get('settings','tm_dt1')
-tm_d3 = config.get('settings','tm_d3')
-tm_d4 = config.get('settings','tm_d4')
-tm_dt2 = config.get('settings','tm_dt2')
 kw_d1 = config.get('settings','kw_d1')
 kw_d2 = config.get('settings','kw_d2')
 kw_dt1 = config.get('settings','kw_dt1')
-kw_d3 = config.get('settings','kw_d3')
-kw_d4 = config.get('settings','kw_d4')
-kw_dt2 = config.get('settings','kw_dt2')
 dic_tm = {kw_d1:tm_d1, kw_d2:tm_d2, kw_dt1:tm_dt1}
-dic_tm_wd = {kw_d3:tm_d3, kw_d4:tm_d4, kw_dt2:tm_dt2}
 tm_mode = ""
 
 # Define sub functions
 def photo_handler(msg, chat_id):
     pho_id = msg['photo'][-1]['file_id']
-    pho_dest = os.path.join('/media/pi/ex500/Pictures', str(msg['date']) + '.jpg')
+    pho_dest = os.path.join('/volume1/PhotoGallery', str(msg['date']) + '.jpg')
     bot.download_file(pho_id, pho_dest)
     bot.sendMessage(chat_id,"Photo is saved as\n" + pho_dest) 
 
@@ -53,21 +44,11 @@ def doc_handler(msg, chat_id):
         elif tm_mode in dic_tm:
             tm_dir = dic_tm.get(tm_mode)
             f_temp = os.path.join(os.path.dirname(os.path.abspath(__file__)),f_name)
-            command = "transmission-remote -n '" + tm_n + "' -a '" + f_temp + "' -w " + tm_dir 
+            command = 'deluge-console "add -p ' + tm_dir + ' ' + f_temp + '"' 
             bot.download_file(f_id, f_temp)
             risp = subprocess.check_output(command, shell=True)
             bot.sendMessage(chat_id, risp)
-        elif tm_mode in dic_tm_wd:
-            tm_dir = dic_tm_wd.get(tm_mode)
-            f_temp = os.path.join(os.path.dirname(os.path.abspath(__file__)),f_name)
-            bot.download_file(f_id, f_temp)
-            cp_command = "scp '" + f_temp + "' sshd@192.168.0.117:" + tm_dir
-            subprocess.run(cp_command, shell=True)
-            command = "ssh -t sshd@192.168.0.117 'transmission-remote localhost:9092 -n " + tm_n_wd + " -a \"" + os.path.join(tm_dir, f_name) + "\" -w \"" + tm_dir + "\"'" 
-            risp = subprocess.check_output(command, shell=True)
-            bot.sendMessage(chat_id, risp)
-            subprocess.run("ssh sshd@192.168.0.117 'rm \"" + os.path.join(tm_dir, f_name) + "\"'", shell=True)
-        subprocess.run("rm '" + f_temp + "'", shell=True)
+            subprocess.call("rm '" + f_temp + "'", shell=True)
         tm_mode = ""
     elif f_type == "application/octet-stream":
         f_ext = os.path.splitext(f_name)[1]
@@ -79,7 +60,7 @@ def doc_handler(msg, chat_id):
 def text_handler(msg, chat_id):
     global tm_mode
     m_text = msg['text'].lower()
-    if m_text in dic_tm or m_text in dic_tm_wd:
+    if m_text in dic_tm:
         tm_mode = m_text
     else:
         command = msg['text']
